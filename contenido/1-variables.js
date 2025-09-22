45787182b50bf16561f28ba82bd25f43f89d52cf
ByronKinal <bpineda-2024427@kinal.edu.gt>
2025-09-22 11:59:10 -0600

    Mi primer proyecto js

---
+++ contenido/1-variables.js
@@ -0,0 +1,19 @@
+//Buenas practicas, ser descriptivo
+let cajaAudifonos = "derecho"
+let urlDeUsuario = "https://www.google.com"
+let carnetAlumno = "2022464"
+
+//Mala practica, no usar variables ambiguas
+let c = "derecho"
+let ca = "derecho"
+let caUsuario = "derecho"
+
+console.log(cajaAudifonos)
+
+//otras formas
+
+var nombre = "Diego"
+nombre = "Carlos"
+var edad
+
+const PI = 3.14159
---
+++ contenido/2-Tipos-de-datos.js
@@ -0,0 +1,46 @@
+/**
+ * TIPOS DE DATOS: (1) primitivos y (2) complejos (10 tipos)
+ */
+
+//(1)PRIMITIVOS
+// string
+let nombre = "Diego"
+
+//number
+let edad = 31
+
+//boolean
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefied
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol("unico")
+
+//bigint
+let numeroGrande = 2n
+
+//console.log(numeroGrande)
+
+//COMPLEJOS
+
+//object
+let carro = {
+    marca: "fiesta",
+    color: "blanco",
+    dueño: "mamon"
+}
+
+//array
+let fruta = ["manzana", "banano", "uvas"]
+
+//function
+function name(params){
+    //codigo
+}
+
+console.log(fruta)
