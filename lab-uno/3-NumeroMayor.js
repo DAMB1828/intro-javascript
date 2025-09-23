a4b2055f04158efc193901675b691d2cd1ace714
DAMB1828 <dmauricio-2022464@kinal.edu.gt>
2025-09-23 11:52:13 -0600

    3-Numero Mayor
    Funcion que recibe dos numeros y retorna el mayor

---
+++ lab-uno/3-NumeroMayor.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ *  Crear una funcion que reciba dos numeros y retorne el numero mayor
+ * 
+ */
+
+function elMayor(primerNumero, segundoNumero) {
+  if (primerNumero > segundoNumero) {
+    return primerNumero
+  } else {
+    return segundoNumero
+  }
+}
+
+let primerNumero = 80
+let segundoNumero = 28
+let numeroMasGrande = elMayor(primerNumero, segundoNumero)
+console.log(`El número más grande entre ${primerNumero} y ${segundoNumero} es: ${numeroMasGrande}`)
