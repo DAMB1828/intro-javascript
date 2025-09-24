43c634dbe39eb9c1b61af10021e0050b403ea1e2
Informatica <dmauricio-2022464@kinal.edu.gt>
2025-09-24 07:53:06 -0600

    26-CantidadDeVocales

---
+++ lab-uno/26-CantidadDeVocales.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ *  CANTIDAD DE VOCALES
+ * 
+ */
+
+function contarVocales(cadena) {
+  const vocales = "aeiouAEIOU";
+  let contador = 0;
+  for (const caracter of cadena) {
+    if (vocales.includes(caracter)) {
+      contador++;
+    }
+  }
+  return contador;
+}
+
+const texto = "Programación en JavaScript";
+const cantidadVocales = contarVocales(texto);
+console.log(`La cantidad de vocales en "${texto}" es: ${cantidadVocales}`);
